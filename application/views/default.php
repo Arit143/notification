d0c3b62a6064c52627d0e0a1bf6aa524d3049ffb
Arit143 <coolarit143@gmail.com>
2016-03-19 01:57:02 +0530

    Include bootstrap and Jquery

--- application/views/default.php
+++ application/views/default.php
@@ -1,1 +1,6 @@
-<script src="/path/to/file.js" type="text/javascript" charset="utf-8"></script>
+<!-- Default CSS-->
+<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+<!-- Default JS files-->
+<script src="bower_components/jquery/dist/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
